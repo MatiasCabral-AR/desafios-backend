cb2f7f711c9f90b3579707935a1a8657e30d1ef3
pagatelabirra <raulmatiascabral@gmail.com>
2022-08-27 18:57:21 -0300

    Terminado funcionamiento de nuevo producto

--- desafio6/public/js/main.js
+++ desafio6/public/js/main.js
@@ -1,21 +1,34 @@
-const socket = io.connect();
+const socket = io();
 
-function render(data){
-    const html = data.map(elem=> {
-        return (`<div class='d-flex justify-content-center align-items-center'><h3 class="text-light px-2">Autor : ${elem.author}</h3><p class='text-light px-2 align-items-center'>Mensaje : ${elem.text}</p></div>`)
-    }).join(' ');
-    document.getElementById('messages').innerHTML = html;
-};
-
-function addMessage(e){
-    const message = {
-        author : document.getElementById('author').value,
-        text : document.getElementById('text').value
-    }
-    socket.emit('new-message', message);
-    return false;
-}
-
-socket.on('messages', data => {
-    render(data);
+const newProduct = document.getElementById('newProduct')
+newProduct.addEventListener('submit', event => {
+  event.preventDefault()
+  let id = document.getElementById('ID').value
+  let name = document.getElementById('NAME').value
+  let price = document.getElementById('price').value
+  let src = document.getElementById('src').value
+  console.log(`${name}, #${id}, $${price}, ${src}`)
+  socket.emit('new_product', {
+    id : id,
+    name : name,
+    price : price,
+    src : src
+  })
+})
+socket.on('connect', () => {
+  console.warn('Conectado al servidor');
 });
+socket.on('update_products', products => {
+  fetch('http://localhost:8080/views/products-render.hbs')
+    .then(response => {
+      return response.text()
+    })
+    .then(plantilla => {
+      const template = Handlebars.compile(plantilla);
+      const html = template({products})
+      document.getElementById('productos').innerHTML = html;
+    })
+})
+socket.on('update_messages', messages => {
+  console.log(messages)
+})
